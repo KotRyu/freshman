35de242c4ee6d20aba10b010377979960f3348a2
ryunosuke_kotaki <ryunosuke.kotaki@mediaseek.co.jp>
2019-12-24 14:32:23 +0900

    初期コミット

---
+++ src/main/java/jp/co/mediaseek/freshman/FreshmanApplication.java
@@ -0,0 +1,13 @@
+package jp.co.mediaseek.freshman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/mediaseek/freshman/FreshmanApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.mediaseek.freshman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
